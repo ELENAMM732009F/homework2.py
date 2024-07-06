749fa9f6e20714297a302b351a54d5eda0783e03
ELENAMM732009F <elenamm732009@gmail.com>
2024-07-06 23:41:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+total_tasks=12
+all_the_time=1.5
+well='Python'
+one_task=float(all_the_time)/int(total_tasks)
+print(well,"вспго задач",total_tasks,'затрачено часов',all_the_time,'среднее время выполнения',one_task)
+
